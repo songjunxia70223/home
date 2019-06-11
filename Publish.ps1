d702b3d6cd8995f7b76efc4dab124ea3270a42e3
song_jx <song_jx@outlook.com>
2019-06-11 16:25:37 +0800

    Update Publish.ps1

--- Publish.ps1
+++ Publish.ps1
@@ -1,7 +1,7 @@
-cp -Force_book\* .\
+Copy-Item -Path _book\* -Destination .\ -Recurse -force 
 git config --global user.name "song_jx"
 git config --global user.email "song_jx@outlook.com"
 git add -A
 git commit -m 'Update GitBook'
 git push origin gh-pages
-----------finish push----------
+"-------------------------------------finish push-------------------------------------"
